88384d2f8e4821c3bd9ef986bb3593998e45ca01
marshallhead <aal029@yahoo.com>
2019-12-07 20:35:19 +0800

    completed storyboard, link all IBoutlets and IBActions

--- Meteo/ViewController.swift
+++ Meteo/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ConditionImageView: UIImageView!
+    @IBOutlet weak var TemperatureLabel: UILabel!
+    @IBOutlet weak var CityLabel: UILabel!
+    
+    
+    @IBOutlet weak var SearchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func locationPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+    }
+    
+    
 }
 
